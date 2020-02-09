ed533a68b3f519d04af67aa51f39696cfb3e28aa
Jeetendra Gund <jsgund@gmail.com>
2020-02-09 14:13:30 +0530

    First option

---
+++ First option/FetchAppsettingsValue/FetchAppsettingsValue/Controllers/ConfigurationController.cs
@@ -0,0 +1,26 @@
+﻿namespace FetchAppsettingsValue.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using System.Collections.Generic;
+
+    [Route("api/Configuration")]
+    [ApiController]
+    public class ConfigurationController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public ConfigurationController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // GET: api/Configuration
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var result = _config.GetValue<string>("Logging:LogLevel:Default");
+            return new string[] { result.ToString() };
+        }
+    }
+}
