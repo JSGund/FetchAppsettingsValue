1c03519a1cd4de61ba09529ec1f05387d4decf03
Jeetendra Gund <jsgund@gmail.com>
2020-02-09 16:50:35 +0530

    Second option

---
+++ Second option/FetchAppsettingsValue/FetchAppsettingsValue/AppSettings.cs
@@ -0,0 +1,22 @@
+﻿namespace FetchAppsettingsValue
+{
+    public class AppSettings
+    {
+        public Logging Logging { get; set; }
+        public string AllowedHosts { get; set; }
+    }
+
+    public class Logging
+    {
+        public LogLevel LogLevel { get; set; }
+    }
+
+    public class LogLevel
+    {
+        public string Default { get; set; }
+
+        public string Warning { get; set; }
+
+        public string Error { get; set; }
+    }
+}
---
+++ Second option/FetchAppsettingsValue/FetchAppsettingsValue/Controllers/OptionController.cs
@@ -0,0 +1,27 @@
+﻿namespace FetchAppsettingsValue.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Options;
+    using System.Collections.Generic;
+
+    [Route("api/Option")]
+    [ApiController]
+    public class OptionController : ControllerBase
+    {
+        private readonly IOptions<Logging> _logging;
+
+        public OptionController(IOptions<Logging> logging)
+        {
+            _logging = logging;
+        }
+
+        // GET: api/Option
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var result = _logging.Value.LogLevel.Default; // "Information"
+
+            return new string[] { result };
+        }
+    }
+}
